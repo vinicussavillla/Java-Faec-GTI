7e8705ac1da8eabdfa7b8a36387454f54a83658e
ViniciusA <66341102+vinicussavillla@users.noreply.github.com>
2021-01-23 20:32:45 -0300

    Add files via upload

---
+++ Atividade01.java
@@ -0,0 +1,26 @@
+package Atividade01;
+
+/**
+ATIVIDADE 01:
+HORA DA REVISÃO
+Crie um programa em java que imprima uma breve apresentação.
+Exemplo:
+"Oi, eu sou o prof. Leandro Reis.
+Eu sou especialista em Engenharia de Software e sou um entusiasta de Inteligência Artificial".
+*/
+
+/**
+ * @author Vinicus  <Vinistaoo@gmail.com>
+ * @date 06/05/2020
+ */
+
+public class Atividade01 {
+    public static void main(String[] args) {
+        System.out.println("Oi, meu nome é Vinicius de Avilla");
+        System.out.println("Sou estudande na área de Gestão da Tecnologia da Iformação");
+    }
+}
+
+
+
+
