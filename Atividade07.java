11ca33fa1e16fe27a7c38238d3e1dfefdc5bfecf
ViniciusA <66341102+vinicussavillla@users.noreply.github.com>
2021-06-24 21:45:02 -0300

    modulo 5 - Técnicas de Programação
    A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,... 
    programa capaz de gerar a série até o n−ésimo termo.

--- Atividade07.java
+++ Atividade07.java
@@ -17,5 +17,32 @@ public class Ativadade07 {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+      private void ExecutarAlgoritmo() {
+       int numero = perguntarNumerodeSequncias();
+       long primeiroNumero = 0;
+       long segundoNumero = 1;
+       int fibonacci;
+       
+       System.out.println("0 \n1");
+       for (int i = 1; i <= numero; i++) {
+            fibonacci = (int) fazerSerieFibonacii(primeiroNumero, segundoNumero);
+            primeiroNumero = segundoNumero;
+            segundoNumero = fibonacci;
+            System.out.println(fibonacci); 
+        }
+    }
+ 
+    private long fazerSerieFibonacii(long primeiroNumero, long segundoNumero) {
+        long numeroSequencia = primeiroNumero + segundoNumero;
+        return numeroSequencia;
+    } 
+
+    private int perguntarNumerodeSequncias() {
+        Scanner read  = new Scanner(System.in);
+        System.out.print("Digite o número de sequências: ");
+        int numero = read.nextInt();
+        return numero;
+    }
+ }
     
 }
